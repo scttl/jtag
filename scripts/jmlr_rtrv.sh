947ae1f62add543473f7be882714ddd610cfbad1
scottl <scottl>
2003-07-14 18:38:34 +0000

    Updated root directory, forced checks to ensure no redownloading of existing articles occured.

--- scripts/jmlr_rtrv.sh
+++ scripts/jmlr_rtrv.sh
@@ -3,18 +3,18 @@
 ##
 ## FILE: jmlr_rtrv.sh
 ##
-## CVS: $Id: jmlr_rtrv.sh,v 1.1 2003-06-03 23:38:06 scottl Exp $
+## CVS: $Id: jmlr_rtrv.sh,v 1.2 2003-07-14 18:38:34 scottl Exp $
 ##
 ## DESCRIPTION: wget script to retrieve all the pdf papers from the 
 ##              jmlr.org website.
 ##
 ################################################################################
 
-ROOT=./jmlr          # base directory in which to place journals and logs
+ROOT=./jmlr_journals # base directory in which to place journals and logs
 LOGNAME=RESULTS.log  # name of output log
 URL=http://www.jmlr.org/papers # base url containing links to articles
 
 # execute the command
-wget -r -nH -nd -P $ROOT -o ${ROOT}/${LOGNAME} -A "*.pdf" $URL/v1 $URL/v2 \
+wget -r -N -nH -nd -P $ROOT -o ${ROOT}/${LOGNAME} -A "*.pdf" $URL/v1 $URL/v2 \
      $URL/v3 $URL/v4 $URL/special
 
