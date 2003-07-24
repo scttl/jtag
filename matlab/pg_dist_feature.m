1b4033ea838debf52697e505eb31851a2aaab2f8
scottl <scottl>
2003-07-24 19:33:23 +0000

    Changed return value to actually be a row vector.  Updated comments to reflect this.

--- matlab/pg_dist_feature.m
+++ matlab/pg_dist_feature.m
@@ -1,8 +1,8 @@
-function [left, top, right, bottom] = pg_dist_feature(rect, pixels)
-% PG_DIST_FEATURE   Returns the distance from the rectangle passed to the edge
+function dists = pg_dist_feature(rect, pixels)
+% PG_DIST_FEATURE   Returns the distance from the rectangle passed to each edge
 %                   of the page.
 %
-%  [L, T, R, B] = PG_DIST_FEATURE(RECT, PAGE)  This feature simply returns a 4
+%  DISTS = PG_DIST_FEATURE(RECT, PAGE)  This feature simply returns a 4
 %  element row vector containing the pixel distance to the left, top, right,
 %  and bottom edges of the page matrix PAGE, starting from the 4 element
 %  column vector RECT passed.
@@ -10,11 +10,15 @@ function [left, top, right, bottom] = pg_dist_feature(rect, pixels)
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: pg_dist_feature.m,v 1.2 2003-07-23 22:28:11 scottl Exp $
+% $Id: pg_dist_feature.m,v 1.3 2003-07-24 19:33:23 scottl Exp $
 % 
 % REVISION HISTORY:
 % $Log: pg_dist_feature.m,v $
-% Revision 1.2  2003-07-23 22:28:11  scottl
+% Revision 1.3  2003-07-24 19:33:23  scottl
+% Changed return value to actually be a row vector.  Updated comments to reflect
+% this.
+%
+% Revision 1.2  2003/07/23 22:28:11  scottl
 % Small bugfix to tighten checks when selection begins at location 1,1.
 %
 % Revision 1.1  2003/07/23 20:30:39  scottl
@@ -38,7 +42,4 @@ elseif rect(1) < 1 | rect(2) < 1 | rect(3) > c | rect(4) > r
     error('RECT passed exceeds PAGE boundaries');
 end
 
-left = rect(1) - 1;
-top = rect(2) - 1;
-right = c - rect(3);
-bottom = r - rect(4);
+dists = [rect(1) - 1, rect(2) - 1, c - rect(3), r - rect(4)];
