36ede5b8b153cc724a079dfe8efef02ee80f5223
scottl <scottl>
2003-07-24 19:24:52 +0000

    Changed threshold to a value between 0 and 1 (not 100). Return a row vector instead of a column vector.

--- matlab/sr_ink_feature.m
+++ matlab/sr_ink_feature.m
@@ -3,20 +3,25 @@ function s = sr_ink_feature(rect, pixels, varargin)
 %                  passed that meets an ink percentage threshold.
 %
 %  SR_INK_FEATURE(RECT, PAGE, {THRESHOLD})  This feature attempts to shrink the 
-%  sides of 4 element column vector RECT passed, working in a clockwise manner,
+%  sides of 4 element vector RECT passed, working in a clockwise manner,
 %  until the number of non-background PAGE pixels counted for the side currently
 %  under consideration is larger than THRESHOLD percentage passed.  If 
 %  THRESHOLD is not specified it defaults to 2 percent of the total pixels of 
-%  that side.
+%  that side. The return value is a 4 element row vector listing the left,
+%  top, bottom, and right pixel co-ords of the subrectangle.
 
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: sr_ink_feature.m,v 1.3 2003-07-23 22:28:51 scottl Exp $
+% $Id: sr_ink_feature.m,v 1.4 2003-07-24 19:24:52 scottl Exp $
 % 
 % REVISION HISTORY:
 % $Log: sr_ink_feature.m,v $
-% Revision 1.3  2003-07-23 22:28:51  scottl
+% Revision 1.4  2003-07-24 19:24:52  scottl
+% Changed threshold to a value between 0 and 1 (not 100).
+% Return a row vector instead of a column vector.
+%
+% Revision 1.3  2003/07/23 22:28:51  scottl
 % Small bugfix for when selection location begins at 1,1.
 %
 % Revision 1.2  2003/07/23 21:31:03  scottl
@@ -46,10 +51,10 @@ elseif rect(1) < 1 | rect(2) < 1 | rect(3) > c | rect(4) > r
 end
 
 if nargin == 3
-    if varargin{1} < 0 | varargin{1} > 100
-        error('THRESHOLD passed must be a percentage (between 0 and 100)');
+    if varargin{1} < 0 | varargin{1} > 1
+        error('THRESHOLD passed must be a percentage (between 0 and 1)');
     end
-    threshold = varargin{1} / 100;
+    threshold = varargin{1};
 end
 
 % loop over the 4 sides, trimming our current bounding box until we have met
@@ -131,5 +136,4 @@ while ~ (l_done & t_done & r_done & b_done)
     
 end  %end while loop
 
-s = [left; top; right; bottom];
-return;
+s = [left top right bottom];
