dd152836d1354cddd33518cf95f8e9a4e796cce6
scottl <scottl>
2003-07-24 19:49:59 +0000

    Changed algorithm to detect whitespace by scanning the entire width of the page for top and bottom whitespace checks, and only scan the height of the selection for the left and right checks. Also changed the return value so that an actual row vector is returned.

--- matlab/rect_outer_ws_feature.m
+++ matlab/rect_outer_ws_feature.m
@@ -1,25 +1,33 @@
-function [left, top, right, bottom] = rect_outer_ws_feature(rect, pixels, ...
-                                                            varargin)
-% RECT_OUTER_WS_FEATURE   Returns the distance from the edges of the RECT
-%                         passed, out to the nearest amounts of substantial 
-%                         ink on each of the 4 sides outside RECT.
+function dists = rect_outer_ws_feature(rect, pixels, varargin)
+% RECT_OUTER_WS_FEATURE   Returns the distance (in pixels) from the edges of 
+%                         the RECT passed, out to the nearest amounts of 
+%                         substantial ink on each of the 4 sides outside RECT.
 %
-%  [L, T, R, B] = RECT_OUTER_WS_FEATURE(RECT, PAGE, {THRESHOLD})  This feature 
+%  DISTS = RECT_OUTER_WS_FEATURE(RECT, PAGE, {THRESHOLD})  This feature 
 %  returns a 4 element row vector containing the pixel distance to the left, 
 %  top, right, and bottom areas of substantial non-background ink inside PAGE.
 %  Substantial is determined by the percentage THRESHOLD passed, and defaults
 %  to 2 if not specified.  Note that if there is only whitespace between a
 %  side of RECT and the edge of the page, the distance returned for that side
-%  is the distance to the edge of the PAGE.
+%  is the distance to the edge of the PAGE.  Also note that to determine
+%  whitespace above and below the selection it looks at the entire width of
+%  the page.  To determine whitespace to the left or right, it only looks out
+%  at the same height as the selection (not above or below it).
 
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: rect_outer_ws_feature.m,v 1.2 2003-07-23 22:28:33 scottl Exp $
+% $Id: rect_outer_ws_feature.m,v 1.3 2003-07-24 19:49:59 scottl Exp $
 % 
 % REVISION HISTORY:
 % $Log: rect_outer_ws_feature.m,v $
-% Revision 1.2  2003-07-23 22:28:33  scottl
+% Revision 1.3  2003-07-24 19:49:59  scottl
+% Changed algorithm to detect whitespace by scanning the entire width of the
+% page for top and bottom whitespace checks, and only scan the height of the
+% selection for the left and right checks.
+% Also changed the return value so that an actual row vector is returned.
+%
+% Revision 1.2  2003/07/23 22:28:33  scottl
 % Small bugfix for when pixel location begins at 1,1.
 %
 % Revision 1.1  2003/07/23 21:31:57  scottl
@@ -47,9 +55,9 @@ end
 
 if nargin == 3 
     if varargin{1} < 0 | varargin{1} > 100 
-        error('THRESHOLD passed must be a percentage (between 0 and 100)'); 
+        error('THRESHOLD passed must be a percentage (between 0 and 1)'); 
     end
-    threshold = varargin{1} / 100;
+    threshold = varargin{1};
 end
 
 left   = rect(1);
@@ -71,12 +79,12 @@ while ~ (l_done & t_done & r_done & b_done)
             l_done = true;
         else
             count = 0;
-            for i = top:bottom
+            for i = rect(2):rect(4)
                 if pixels(i, left) ~= bg
                     count = count + 1;
                 end
             end
-            if count > (threshold * (bottom - top + 1))
+            if count > (threshold * (rect(4) - rect(2) + 1))
                 l_done = true;
             end
         end
@@ -89,12 +97,12 @@ while ~ (l_done & t_done & r_done & b_done)
             t_done = true;
         else
             count = 0;
-            for i = left:right
+            for i = 1:c
                 if pixels(top,i) ~= bg
                     count = count + 1;
                 end
             end
-            if count > (threshold * (right - left + 1))
+            if count > (threshold * c)
                 t_done = true;
             end
         end
@@ -107,12 +115,12 @@ while ~ (l_done & t_done & r_done & b_done)
             r_done = true;
         else
             count = 0;
-            for i = top:bottom
+            for i = rect(2):rect(4)
                 if pixels(i, right) ~= bg
                     count = count + 1;
                 end
             end
-            if count > (threshold * (bottom - top + 1))
+            if count > (threshold * (rect(4) - rect(2) + 1))
                 r_done = true;
             end
         end
@@ -125,12 +133,12 @@ while ~ (l_done & t_done & r_done & b_done)
             b_done = true;
         else
             count = 0;
-            for i = left:right
+            for i = 1:c
                 if pixels(bottom, i) ~= bg
                     count = count + 1;
                 end
             end
-            if count > (threshold * (right - left + 1))
+            if count > (threshold * c)
                 b_done = true;
             end
         end
@@ -138,3 +146,4 @@ while ~ (l_done & t_done & r_done & b_done)
     
 end  %end while loop
 
+dists = [left, top, right, bottom];
