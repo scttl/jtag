eea02def86d216a6f1ba82e7839613070780223c
klaven <klaven>
2004-06-08 00:58:03 +0000

    Outputs testing results to a csv file

---
+++ matlab/test_print.m
@@ -0,0 +1,19 @@
+function rr = test_print(results,labels,fpath);
+
+fid = fopen(fpath,'w');
+
+for i=1:length(labels);
+  fprintf(fid,',%s',labels{i});
+end;
+fprintf(fid,'\n');
+
+for i=1:size(results,2);
+  fprintf(fid,'%s',labels{i});
+  for j=1:size(results,1);
+    fprintf(fid,',%i',results(j,i));
+  end;
+  fprintf(fid,'\n');
+end;
+
+fclose(fid);
+rr = 0;
