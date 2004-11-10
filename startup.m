2450cd9316a9b85a7451c2a90c3731cefadb83c6
klaven <klaven>
2004-11-10 22:21:14 +0000

    Added GPL OCR package gocr

--- startup.m
+++ startup.m
@@ -18,4 +18,10 @@ class_names = {'text' 'authour_list' 'section_heading' 'main_title' ...
                'image' 'bullet_item' 'code_block' 'figure' ...
                'figure_label' 'table_label' 'header' 'editor_list' ...
                'pg_number' 'footer' 'start_of_page' 'end_of_page' };
-
+global use;
+use.dist = true;
+use.snap = false;
+use.pnum = true;
+use.dens = true;
+use.mark = true;
+use.ocr = false;
